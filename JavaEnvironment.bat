0192f792aef2d44b22684953c5c7190c6f0bebbe
coderalexis <j.a.c.solar95@gmail.com>
2024-01-03 23:06:57 -0600

    Updated script to handle new Java naming conventions

--- JavaEnvironment.bat
+++ JavaEnvironment.bat
@@ -4,8 +4,8 @@ setlocal enabledelayedexpansion
 echo.
 echo Seleccione una version de Java:
 echo ===============================
-echo 1. Java 8 (última versión)
-echo 2. Java 11 (última versión)
+echo 1. Java 8
+echo 2. Java 11
 echo 3. Java 17
 echo 4. Salir
 echo.
@@ -20,52 +20,20 @@ pause
 exit
 
 :java8
-set "latestJava8Version=000"
-set "latestJava8Path="
-
-for /f "delims=" %%d in ('dir /b /ad "C:\Program Files\Java\jdk1.8.0_*"') do (
-    set "currentVersion=%%~nxd"
-    set "currentVersion=!currentVersion:~-3!"
-    if !currentVersion! gtr !latestJava8Version! (
-        set "latestJava8Version=!currentVersion!"
-        set "latestJava8Path=C:\Program Files\Java\%%d"
-    )
-)
-
-if not "!latestJava8Path!"=="" (
-    echo Cambiando JAVA_HOME a !latestJava8Path!...
-    setx JAVA_HOME "!latestJava8Path!" /M
-    echo Cambio completado a Java 8.
-    echo JAVA_HOME configurado es:
-    echo !latestJava8Path!
-) else (
-    echo No se encontró una instalación de Java 8.
-)
+echo Cambiando a Java 8...
+setx JAVA_HOME "C:\Program Files\Java\jdk-1.8" /M
+echo Cambio completado a Java 8.
+echo JAVA_HOME configurado es:
+echo C:\Program Files\Java\jdk-8
 pause
 exit
 
 :java11
-set "latestJava11Version=000"
-set "latestJava11Path="
-
-for /f "delims=" %%d in ('dir /b /ad "C:\Program Files\Java\jdk-11.0.*"') do (
-    set "currentVersion=%%~nxd"
-    set "currentVersion=!currentVersion:~-2!"
-    if !currentVersion! gtr !latestJava11Version! (
-        set "latestJava11Version=!currentVersion!"
-        set "latestJava11Path=C:\Program Files\Java\%%d"
-    )
-)
-
-if not "!latestJava11Path!"=="" (
-    echo Cambiando JAVA_HOME a !latestJava11Path!...
-    setx JAVA_HOME "!latestJava11Path!" /M
-    echo Cambio completado a Java 11.
-    echo JAVA_HOME configurado es:
-    echo !latestJava11Path!
-) else (
-    echo No se encontró una instalación de Java 11.
-)
+echo Cambiando a Java 11...
+setx JAVA_HOME "C:\Program Files\Java\jdk-11" /M
+echo Cambio completado a Java 11.
+echo JAVA_HOME configurado es:
+echo C:\Program Files\Java\jdk-11
 pause
 exit
 
